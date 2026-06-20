36e364daaf4bb6c7cba34ca605a557be3599a768
GobikaT <gobikathuyamani@gmail.com>
2026-06-20 13:42:47 +0530

    changes commited

--- MODULE_2/Exercise1(Inventory)/InventoryMangement/src/App.java
+++ MODULE_2/Exercise1(Inventory)/InventoryMangement/src/App.java
@@ -3,7 +3,6 @@ public class App {
         Inventory obj=new Inventory();
        Product p1 = new Product(101, "Laptop", 50000, 10);
         Product p2=new Product(102,"Mouse",4000,8);
-        Product p3=new Product(103,"Motherboard",4458,14);
         obj.addproduct(p1);
         obj.addproduct(p2);
         obj.updateProduct(101, "Keyboard", 4000, 3);
